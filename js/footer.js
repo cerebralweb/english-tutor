3873a74754b684627dc79185a91aace1cc302d65
Paulo C. Leon <paulocleon@gmail.com>
2025-07-14 13:33:40 -0600

    hello

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   var footerHTML = `
     <footer>
-      <p>&copy; 2025 CL Palacios. Aprende desde cualquier lugar. Learn from anywhere.</p>
+      <p>&copy; 2025 CL Palacios, Morelia, Michoacán. Aprende desde cualquier lugar. Learn from anywhere.</p>
     </footer>
   `;
   document.body.insertAdjacentHTML('beforeend', footerHTML);
